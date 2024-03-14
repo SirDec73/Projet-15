12f249f8d3a3d2abc5a4c046dfc606a78a6577d6
SirDec73 <147041546+SirDec73@users.noreply.github.com>
2024-03-14 10:11:04 +0100

    Update main.c

--- Plus_Ou_Moins2.0/main.c
+++ Plus_Ou_Moins2.0/main.c
@@ -92,12 +92,12 @@ int main()
         int Nb = rand() % (Bmax - Bmin + 1) + Bmin;
         printf("Le nombre random %d\n", Nb);
 
-        //Tant qu'il y a des coups à jouer et que le nombre n'est pas encore decouvert
+        //Tant qu'il y a des coups Ã  jouer et que le nombre n'est pas encore decouvert
         while (AskNb != Nb && Tryuse < Try)
         {
             //Demande un nombre
             AskNb = AskInt("Choisi un nombre : ", AskNb, Bmin, Bmax);
-            //Differentes possibilitées
+            //Differentes possibilitÃ©es
             if (Nb < AskNb) {
                 printf("Moins\n");
             }
@@ -132,144 +132,3 @@ int main()
     }
 
 }
-/*
-// Exo:
-void functionExo3(float* tableau) {
-    for (int i = 0; i < 10; i++) {
-        tableau[i] = 5;
-    }
-}
-void functionExo4(float* tableau) {
-    for (int i = 0; i < 10; i++) {
-        tableau[i] = 5;
-    }
-}
-
-int functionExo5(int Value, int Bmin, int Bmax) {
-    int iError = 0;
-    while (iError == 0) {
-        if (scanf_s("%d", &Value)) {
-            if (Value <= Bmax && Value >= Bmin) {
-                return Value;
-            }
-        }
-        char c = getchar();
-        while (c != '\n') { c = getchar(); }
-    }
-}
-int functionExo6(int Value, int Bmin, int Bmax) {
-    int iError = 0;
-    while (iError == 0) {
-        if (scanf_s("%d", &Value)) {
-            if (Value <= Bmax && Value >= Bmin) {
-                return Value;
-            }
-        }
-        char c = getchar();
-        while (c != '\n') { c = getchar(); }
-    }
-}
-
-
-int main() {
-    // Exo:1
-    printf("Exercice 1");
-    float f1 = 5;
-    float* pf = &f1;
-    *pf = 10;
-    float f2;
-    pf = &f2;
-    *pf = 20;
-    printf("%f\n", f1);
-    printf("%f\n", f2);
-
-    // Exo:2
-    printf("\nExercice2\n");
-    // A
-    float* pf1 = (float*)malloc(sizeof(float));
-    if (pf1 == NULL) {
-        exit(1);
-    }
-    // B
-    *pf1 = 5;
-    // C
-    float* pf2 = (float*)malloc(sizeof(float));
-    if (pf2 == NULL) {
-        exit(1);
-    }
-    // D
-    float* pf3;
-    pf3 = pf2;
-    pf2 = pf1;
-    pf1 = pf3;
-
-    printf("pf2 : %f\n", *pf2);
-    printf("pf1 : %f\n", *pf1);
-    //E
-    free(pf2);
-    free(pf1);
-    // Exo:3
-    printf("\nExercice3\n");
-    float* tf = (float*)malloc(sizeof(float) * 10);
-    if (tf == NULL) {
-        exit(1);
-    }
-    for (int i = 0; i < 10; i++) {
-        tf[i] = 0;
-    }
-    functionExo3(tf);
-    for (int i = 0; i < 10; i++) {
-        printf("%d | %f\n", i, tf[i]);
-    }
-    free(tf);
-    // Exo:4
-    printf("\nExercice4\n");
-    float tff[10];
-    for (int i = 0; i < 10; i++) {
-        tff[i] = 0;
-    }
-    functionExo4(tff);
-    for (int i = 0; i < 10; i++) {
-        printf("%d | %f\n", i, tff[i]);
-    }
-    // Exo:5
-
-    printf("\nExercice5\n");
-    int Size = 0;
-    printf("Choisi une taille de tableau : ");
-    scanf_s("%d", &Size);
-    int* tfff = (int*)malloc(sizeof(int) * Size);
-    if (tfff == NULL) {
-        exit(1);
-    }
-
-    for (int i = 0; i < Size; i++) {
-        printf("Choisi un nombre entre 1 et 10000 pour la case n°%d : ", i + 1);
-        tfff[i] = functionExo5(tfff[i], 1, 10000);
-    }
-    for (int i = 0; i < Size; i++) {
-        printf("[%d] => %d\n", i, tfff[i]);
-    }
-    // Exo:6
-    printf("\nExercice6\n");
-    int i = 0;
-    int* tableau6 = (int*)malloc(sizeof(int));
-    if (tableau6 == NULL) {
-        exit(1);
-    }
-
-    while (1) {
-        printf("[%d] => ", i);
-        tableau6[i] = functionExo5(tableau6[i], -1, 10000);
-        printf("|%d|\n", tableau6[i]);
-        if (tableau6[i] == -1) {
-            break;
-        }
-        i++;
-        tableau6 = (int*)realloc(tableau6, sizeof(int) * (i + 1));
-    }
-    for (int k = 0; k < i; k++) {
-        printf("[%d] => %d\n", k, tableau6[k]);
-    }
-    free(tableau6);
-}*/;
